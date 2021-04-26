9a201bc022fcd67d7b6ae0450454330ce07283d9
GianlucaBar <gianlucabarce@gmail.com>
2021-04-26 15:23:42 +0200

    added dark-mode - completed

--- js/script.js
+++ js/script.js
@@ -210,15 +210,3 @@ var app = new Vue(
 
     });
 
-//     const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-
-//     function switchTheme(e) {
-//     if (e.target.checked) {
-//         document.documentElement.setAttribute('data-theme', 'dark');
-//     }
-//     else {
-//         document.documentElement.setAttribute('data-theme', 'light');
-//     }    
-// }
-
-// toggleSwitch.addEventListener('change', switchTheme, false);
